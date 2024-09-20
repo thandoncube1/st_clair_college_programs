8aa1a071865f4c8333b00c9caddeb2939ea474ff
thandoncube1 <ncubethando20@gmail.com>
2024-09-20 11:39:32 -0400

    Adding the logic for handling loan approval and denial

--- GNB_BANK/LoanEstimator.java
+++ GNB_BANK/LoanEstimator.java
@@ -7,13 +7,11 @@ public class LoanEstimator {
         Scanner input = new Scanner(System.in);
         final double prime = 2;
         int creditScore;
-        double interest;
+        double interest = 0;
         double amountBorrowing;
+        boolean loanApproved = true;
 
-        System.out.println("""
-                Welcome to GNB Loan Calculator
-                Please enter your credit score:
-                """);
+        System.out.print("Welcome to GNB Loan Calculator \nPlease enter your credit score: ");
         creditScore = input.nextInt();
         System.out.println("Please enter the amount you'd like to borrow:");
         amountBorrowing = input.nextDouble();
@@ -28,6 +26,18 @@ public class LoanEstimator {
         } else if (creditScore >= 501) {
             if (amountBorrowing < 30000) interest = prime + 2;
             else interest = prime + 5;
-        } else interest = prime + 5;
+        } else {
+            if (amountBorrowing < 30000) interest = prime + 5;
+            else loanApproved = false;
+        }
+
+        if (loanApproved) {
+            double amountPaidOver5Years = amountBorrowing * (interest/100) * 5 + amountBorrowing;
+            double amountPaidOver10Years = amountBorrowing * (interest/100) * 10 + amountBorrowing;
+            System.out.printf("You've been approved for $%,3.2f ", amountBorrowing);
+            System.out.printf(" your interest rate will be %.2f%s\n", interest, "%");
+            System.out.printf("After 5 years you'll have paid $%,3.2f\n", amountPaidOver5Years);
+            System.out.printf("After 10 years you'll have paid $%,3.2f", amountPaidOver10Years);
+        } else System.out.println("Your loan was not approved");
     }
 }

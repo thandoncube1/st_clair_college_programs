ad25ea459ac549ca25dcddbafec2d5bf4d9799a8
thandoncube1 <ncubethando20@gmail.com>
2024-09-20 11:40:31 -0400

    finishing the comparator two for this current

--- Calculator/ComparatorTwo.java
+++ Calculator/ComparatorTwo.java
@@ -10,8 +10,8 @@ public class ComparatorTwo {
         Scanner input = new Scanner(System.in);
         System.out.print("""
                 This program will compare 2 numbers to each other and output the following:
-                1) Is the first number greater than the second 
-                2) Is the first number less than the second 
+                1) Is the first number greater than the second
+                2) Is the first number less than the second
                 3) Is the first number equal to the second
                 """);
         System.out.print("Please enter the first number: ");
@@ -23,17 +23,6 @@ public class ComparatorTwo {
         else if (num1 < num2) System.out.print("The first number is less than the second");
         else if (num1 == num2) System.out.print("The first number is equal to the second");
         else System.out.print("Some how there is no comparison for the two");
-
-
-        System.out.println("Enter a name: ");
-        String str1 = input.toString();
-        System.out.println("Enter a name 2: ");
-        String str2 = input.toString();
-
-        System.out.print(System.identityHashCode(str1));
-        System.out.print(System.identityHashCode(str2));
-        System.out.print(str1.hashCode());
-        System.out.print(str2.hashCode());
 
     }
 }

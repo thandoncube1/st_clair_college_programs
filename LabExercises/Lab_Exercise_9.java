2b152450f3b172da5d2710a2602f51a844b56c5b
thandoncube1 <ncubethando20@gmail.com>
2024-10-02 00:19:09 -0400

    Simplified: Working on a more concise version of the Game.

--- LabExercises/Lab_Exercise_9.java
+++ LabExercises/Lab_Exercise_9.java
@@ -10,23 +10,36 @@ public class Lab_Exercise_9 {
     // From the for use modulo to get out of the 52 select 1 to 13
     // ~ Ace to King ~ match with suit when picking a card.
     int cards = random.nextInt(52) + 1;
-    int result = (cards / 13) + 1;
-    int cardSuit = random.nextInt(result);
-    String suit;
+    int option = (cards / 13) + 1;
+    int cardSuit = random.nextInt(option);
+    int cardDeck = (cards % 13) + 1;
+    String suit = new String();
+    String deck = new String();
     // If statement to get the suit
-    if (cardSuit == 1) {
-      suit = "Diamond";
-    } else if (cardSuit == 2) {
-      suit = "Heart";
-    } else if (cardSuit == 3) {
-      suit = "Club";
-    } else {
-      suit = "Spade";
+    switch (cardSuit) {
+     case 1 -> suit = "Diamond";
+     case 2 -> suit = "Heart";
+     case 3 -> suit = "Club";
+     default -> suit = "Spade";
     }
 
-
-
-    System.out.printf("You picked %ss", suit);
-
+    // This is the switch for selecting cards in the deck.
+    switch (cardDeck) {
+      case 1 -> deck = "Ace";
+      case 2 -> deck = "2";
+      case 3 -> deck = "3";
+      case 4 -> deck = "4";
+      case 5 -> deck = "5";
+      case 6 -> deck = "6";
+      case 7 -> deck = "7";
+      case 8 -> deck = "8";
+      case 9 -> deck = "9";
+      case 10 -> deck = "10";
+      case 11 -> deck = "Jack";
+      case 12 -> deck = "Queen";
+      default -> deck = "King";
+    }
+    // Print to the display what you picked.
+    System.out.printf("You picked %s of %ss", deck, suit);
   }
 }

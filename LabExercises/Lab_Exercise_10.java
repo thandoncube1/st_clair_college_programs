311e25472038c3d439f5120d332145a048f11841
thandoncube1 <ncubethando20@gmail.com>
2024-10-03 12:50:57 -0400

    MAD100: Updating the files for Java

--- LabExercises/Lab_Exercise_10.java
+++ LabExercises/Lab_Exercise_10.java
@@ -4,22 +4,22 @@ package LabExercises;
 import java.util.*;
 
 public class Lab_Exercise_10 {
-  public static void main(String[] args) {
-    // Setup the Random class
-    Random random = new Random();
-    // Declare a heads and tails count and the random coin flip.
-    int numberOfHeads = 0, numberOfTails = 0, randCoinFlip = 0;
+    public static void main(String[] args) {
+      // Set up the Random class
+        Random random = new Random();
+        // Declare a heads and tails count and the random coin flip.
+        int numberOfHeads = 0, numberOfTails = 0, randCoinFlip = 0;
 
-    for (int i = 0; i < 1000000; i++) {
-      randCoinFlip = random.nextInt(2) + 1;
-      // Check the different conditions and environ
-      if (randCoinFlip == 1) {
-         numberOfHeads++;
-      } else if (randCoinFlip == 2) {
-         numberOfTails++;
-      }
+        for (int i = 0; i < 1000000; i++) {
+            randCoinFlip = random.nextInt(2) + 1;
+            // Check the different conditions and environment
+            if (randCoinFlip == 1) {
+               numberOfHeads++;
+            } else if (randCoinFlip == 2) {
+               numberOfTails++;
+            }
+        }
+
+        System.out.println("Number of HEADS: " + numberOfHeads + "\nNumber of TAILS: " + numberOfTails);
     }
-
-    System.out.println("Number of HEADS: " + numberOfHeads + "\nNumber of TAILS: " + numberOfTails);
-  }
 }

5b6bf511fe5699fba8c5a448a0e934d8f6368803
thandoncube1 <ncubethando20@gmail.com>
2024-10-07 11:15:10 -0400

    Lab 13: Working on the display pattern.

---
+++ LabExercises/Lab_Exercise_13.java
@@ -0,0 +1,13 @@
+package LabExercises;
+
+public class Lab_Exercise_13 {
+  public static void displayPattern(int n) {
+    for (int i = 0; i <= n; i++) {
+      System.out.printf("%"+ (n-i+1) + "s", " ");
+      for (int j = i; j > 0; j--) {
+        System.out.print(j);
+      }
+      System.out.println();
+    }
+  }
+}

431fd438bd77862827e2f7f2feb535df9e824201
thandoncube1 <ncubethando20@gmail.com>
2024-10-10 11:14:55 -0400

    Python: Updating the python min max.

--- Week6Programs/exercise1.py
+++ Week6Programs/exercise1.py
@@ -1,6 +1,15 @@
-def min_max(list: list) -> list[int, int]:
+def min_max(values: list) -> list[int, int]:
     if (len(list) < 2):
         return
     current_min = values[0]
     current_max = values[1]
-    
+    right = 0
+    left = 0
+    for i in values:
+        if (i < values[right]):
+            right -= 1
+            pass
+        if (i > values[left]):
+            left -= 1
+            pass
+        

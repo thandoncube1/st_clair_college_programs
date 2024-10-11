0286d16c70aad70b5c4f173b62cf7e4ef90684d7
thandoncube1 <ncubethando20@gmail.com>
2024-10-11 04:02:11 -0400

    Python: Min-Max - Working on the two pointer approach to solving the min-max

--- Week6Programs/exercise1.py
+++ Week6Programs/exercise1.py
@@ -1,21 +1,25 @@
+# This is a Two pointer approach to solving this problem
+# Returning two values min and max
 def min_max(values: list) -> list[int, int]:
+    # If out values array have only 2 elements
     if (len(values) < 2):
         return
-    current_min = values[0]
-    current_max = values[1]
-    right = len(values) - 1
-    left = 0
-    # Get the min value of list
-    while right > left:
-        if (values[left] < values[right]):
-            current_min = values[right]
-            right -= 1
-        elif (values[left] > values[right]):
-            current_max = values[left]
-            right -= 1
-        else: 
-            left += 1
+
+    current_min = current_max = values[0]
+    i = j = 0
+
+    # Check the min and max value of list
+    # by evaluating if current_min or max are greater or less than value.
+    while i < len(values) and j < len(values):
+        if (values[i] < current_min):
+            current_min = values[i]
+        if (values[j] > current_max):
+            current_max = values[j]
+        i += 1
+        j += 1
+
     return [current_min, current_max]
-        
-result = min_max([2, 3, 6, 8, 4, 1])
+
+
+result = min_max([2, 3, 78, 6, 8, 4, 1, 45, 0.89])
 print(result)

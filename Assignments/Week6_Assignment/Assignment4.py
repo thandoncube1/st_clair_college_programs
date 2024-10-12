07406c43bb15583c404bca1ee67ca3630c5b7747
thandoncube1 <ncubethando20@gmail.com>
2024-10-12 03:37:32 -0400

    Python: Working on assignment 4 for week 6

--- Assignments/Week6_Assignment/Assignment4.py
+++ Assignments/Week6_Assignment/Assignment4.py
@@ -1,11 +1,41 @@
-from calculate_module import square_properties as square
-""" 
+from calculate_module import square_properties, rectangle_properties
+"""
     Author: Thando Ncube
     Program: Calculate Area, Perimeter of Square & Rectangle
     Assignment 4 - square_properties and rectangle properties
+    Date: 11 October 2024
 """
+# Display message function
+
+
+def displayMessage(shape, area, perimeter, length, width=0):
+    """
+    `:param shape` - Takes a shape of type string parameter
+    `:param area` - Calculated area value to be displayed
+    `:param perimeter` - Calculated perimeter value of the shape
+    `:param length` - The measurement of the length of side
+    `:param width [optional]` - The measurement of width of the shape
+    `@description` - This is just a display message method that is \n
+    presentational and does not mutate any of the input values.
+    """
+    if shape == "square":
+        print(f"{shape.capitalize()} with side length ", end="")
+        print(f"{length}:\nArea: {area}, Perimeter: {perimeter}")
+        print("--"*20)
+    else:
+        print(f"{shape.capitalize()} with length {length} and ", end="")
+        print(f"Width {width}:\nArea: {area}, Perimeter: {perimeter}")
+        print("--"*20)
+
+
 
 # Calculate the area of a square
-side = 4
-[area, perimeter] = square(side)
-print(f"Square with side length: {side}\nArea: {area}, Perimeter: {perimeter}")
+# Execute the main to test the output of the file
+if __name__ == "__main__":
+    # Initialize the square side
+    [sq_area, sq_perimeter] = square_properties(4)
+    # Initialize the rectangle length & width
+    [r_area, r_perimeter] = rectangle_properties(8, 4)
+    # Display message passing the relevant arguments
+    displayMessage("square", sq_area, sq_perimeter, 4)
+    displayMessage("rectangle", r_area, r_perimeter, 8, 4)
--- Assignments/Week6_Assignment/calculate_module.py
+++ Assignments/Week6_Assignment/calculate_module.py
@@ -1,5 +1,10 @@
 import math
-def square_properties(side: int) -> list:
+
+# Author - Thando Ncube 0875297
+# Module Shapes utilities
+# Assignment 4
+
+def square_properties(side: int | float) -> list:
     """Information about the square property function \n
         `:param side - int` - An integer or float represents the side length of a square.\n
         `:return int` - The area of the square (calculated as the side length squared).\n
@@ -8,9 +13,31 @@ def square_properties(side: int) -> list:
         contains two functions the area and perimeter function 
     """
     def area(side: float) -> float:
+        # Using Math library to calculate the side sqaured
         return math.pow(side, 2)
-    
+
     def perimeter(side: int) -> int:
         return side * 4
-    
+    # Return a list of the functions - pass the parameters
+    # This wil allow us to destructure the results.
     return [area(side), perimeter(side)]
+
+
+def rectangle_properties(length: float, width: float) -> list:
+    """Information about the square property function \n
+    `:param length` - This is a parameter with varying type types `int | float`\n
+    `:param width` - This is another parameter necessary for calculating the area \n
+     of a rectangle\n
+     `return list` - The perimeter of the rectangle (calculated as 2 times the sum\n of the
+length and width).
+    `@description` - The rectangle properties function calculates the area and perimeter \n
+    of a rectangle and returns `[area, perimeter]` result.
+    """
+    def area(length: float, width: float) -> float:
+        return length * width
+
+    def perimeter(length: float, width: float) -> float:
+        return (length + width) * 2
+    # Return a list of functions - passing the parameters from the parent
+    return [area(length, width), perimeter(length, width)]
+
